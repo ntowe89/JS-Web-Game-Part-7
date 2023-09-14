b39dd867d7bea4b9df5cb83622168599b713a847
Nathan Towe <nathanwtowe89@gmail.com>
2023-09-13 22:13:07 -0400

    finished assignment

--- index.js
+++ index.js
@@ -5,7 +5,34 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+//await npc.walkEast(2000)
+
+//console.log(npc.walkEast(2000))
+
+//npc.walkSouth(2000)
+
+// async function moveNPC(){
+//     await npc.walkEast(2000)
+//     npc.walkSouth()
+// }
+// moveNPC()
+
+//test each walk function
+//npc.walkNorth(2000)
+//npc.walkSouth(2000)
+//npc.walkWest(2000)
+
+async function moveNpc(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
+moveNpc()
 
 // Create the inventory
 const inventory = newInventory()
